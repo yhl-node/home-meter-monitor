36198e268dd8e2a5ee0c69611b459a2b2f726528
qingwen.ye <yhl@1024hw.cn>
2018-07-31 00:26:55 +0800

    update

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ const cron = require('node-cron')
 const config = require('config')
 const Meter = require('./meter/')
 
-cron.schedule('* * * * *', function () {
+cron.schedule('0 0 */2 * * *', function () {
   Meter.checkMeter(config.mid)
 })
